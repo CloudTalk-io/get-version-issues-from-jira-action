97e92b4773ea9b9167eeac19b0ffed9bb50882da
Tomas Sykora <tomas.sykora@cloudtalk.io>
2023-02-01 15:34:26 +0100

    feat(MAP-90): generalize code

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const core = require('@actions/core');
 
 try {
     const jiraToken = core.getInput('jira-api-token');
+    const jiraCompanyUrl = core.getInput('jira-company-url');
     const project = core.getInput('project');
     const versionKey = core.getInput('version-key');
     const version = core.getInput('version');
 
     fetch(
-        `https://cloudtalk.atlassian.net/rest/api/3/search?jql=project = ${project} AND ${versionKey} = "${version}" order by created DESC&fields=summary`,
+        `${jiraCompanyUrl}/rest/api/3/search?jql=project = ${project} AND ${versionKey} = "${version}" order by created DESC&fields=summary`,
         {
             method: 'GET',
             headers: {
@@ -23,7 +24,7 @@ try {
 
             const issueList = response.issues.map(
                 (issue) =>
-                    `- <https://cloudtalk.atlassian.net/browse/${issue.key}|${issue.key}> - ${issue.fields.summary}`
+                    `- <${jiraCompanyUrl}/browse/${issue.key}|${issue.key}> - ${issue.fields.summary}`
             );
             core.setOutput("issues", issueList.join('\\n'));
         })
